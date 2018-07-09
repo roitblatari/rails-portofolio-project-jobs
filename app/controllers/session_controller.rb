e1a47297151618ec9faff9e1fe8cada5e2f2c7cc
roitblatari <roitblatari@gmail.com>
2018-07-09 12:52:29 -0400

    working on omniauth

---
+++ app/controllers/session_controller.rb
@@ -0,0 +1,38 @@
+class SessionController < ApplicationController
+  def new
+  # binding.pry
+  end
+
+  def create
+    if params[:provider] == 'github'
+      #create or find user with github emaila and log them in
+      #email = auth[:info][:email]
+      @employer = Employer.find_or_create_by(email: auth['email']) # do |u|
+      @employer.name = auth['info']['name']
+      @employer.uid = auth['info']['uid']
+      # @employer.save #create should do this?
+      @employer.save
+      # binding.pry
+        # u.name = auth['info']['name']
+        # u.email = auth['info']['email']
+        # u.image = auth['info']['image']
+
+      # end
+      # binding.pry
+    # session[:user_id] = @user.id
+      session[:employer_id] = @employer.id
+    redirect_to employer_path(@employer)
+      #
+      # # render 'welcome/home'
+    end
+    # binding.pry
+  end
+
+  def destroy
+  end
+
+private
+  def auth
+    request.env['omniauth.auth']
+  end
+end
