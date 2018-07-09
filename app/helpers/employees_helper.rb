fec9248660158cdacfcd72087a2762131481619a
roitblatari <roitblatari@gmail.com>
2018-07-09 10:35:53 -0400

    current_user

--- app/helpers/employees_helper.rb
+++ app/helpers/employees_helper.rb
@@ -1,5 +1,5 @@
 module EmployeesHelper
   def current_user
-    @current_user ||= Employee.find_by_id(session[:employee_id]) unless session[:mployee_id] == nil
+    @current_user ||= Employee.find_by_id(session[:employee_id]) unless session[:employee_id] == nil
   end
 end
