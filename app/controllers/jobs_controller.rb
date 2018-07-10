514b1b42df917a5f30ac88d89b23641a4fc2cbe0
roitblatari <roitblatari@gmail.com>
2018-07-10 00:00:29 -0400

    new method

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,2 +1,18 @@
 class JobsController < ApplicationController
+  # before_action :employer, only: [new]
+
+  def new
+    @employer = Employer.find_by(id: params[:employer_id])
+    binding.pry
+    @job = @employer.jobs.new
+    # binding.pry
+  end
+
+private
+
+
+  # def employer
+  #   @employer = Employer.find_by(id: params[:employer_id])
+  # end
+
 end
