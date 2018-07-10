6d0c8c1518c4737bf3bc556cbf3bb7916cc92aa6
roitblatari <roitblatari@gmail.com>
2018-07-09 23:59:47 -0400

    in middle of working on the create method

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -7,21 +7,24 @@ class SessionController < ApplicationController
     if params[:provider] == 'github'
       #create or find user with github emaila and log them in
       #email = auth[:info][:email]
-      @employer = Employer.find_or_create_by(email: auth['email']) # do |u|
-      @employer.name = auth['info']['name']
-      @employer.uid = auth['info']['uid']
+      @employer = Employer.find_or_create_by(email: auth['info']['email'])  do |u|
+      # @employer.name = auth['info']['name']
+      # @employer.uid = auth['uid']
+      # @employer.password = SecureRandom.hex
+
       # @employer.save #create should do this?
-      @employer.save
+      # @employer.save
       # binding.pry
-        # u.name = auth['info']['name']
-        # u.email = auth['info']['email']
+        u.name = auth['info']['name']
+        u.email = auth['info']['email']
+        u.password = SecureRandom.hex
         # u.image = auth['info']['image']
 
-      # end
+      end
       # binding.pry
-    # session[:user_id] = @user.id
+
       session[:employer_id] = @employer.id
-    redirect_to employer_path(@employer)
+      redirect_to employer_path(@employer)
       #
       # # render 'welcome/home'
     end
