0bb56983d4e2fa4adec724bfddff9a4be1437777
roitblatari <roitblatari@gmail.com>
2018-07-12 21:02:43 -0400

    current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,12 +5,16 @@ class ApplicationController < ActionController::Base
   # end
 
 
-  # def current_employee
-  #   @current_uemployee ||= Employee.find_by_id(session[:employee_id]) unless session[:employee_id] == nil
-  # end
+  def current_user
+     Employee.find_by_id(session[:employee_id]) || Employer.find_by_id(session[:employer_id])
+  end
 
   # private
+     
+   
+      
 
+    # @current_uemployee ||= Employee.find_by_id(session[:employee_id]) unless session[:employee_id] == nil
   #   def is_logged_in
   #     redirect_to root_path  unless current_user
   #   end
