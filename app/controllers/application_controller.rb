999be15b73b07abe16ac55e0851802d4d7055557
roitblatari <roitblatari@gmail.com>
2018-07-16 10:37:38 -0400

    current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,24 +6,34 @@ class ApplicationController < ActionController::Base
 
 
   def current_user
-    if  Employee.find_by(id: session[:employee_id])
-      @employee = current_user
-    elsif Employer.find_by(id: session[:employer_id])
-      @employer = current_user
-    else
-      redirect_to root_path
-    end
-
+     Employee.find_by_id(session[:employee_id]) || Employer.find_by_id(session[:employer_id])
   end
 
-  # private
+
+  # def current_user
+  #   if @current_user != nil || @current_user != ""
+  #     @current_user
+  #   else
+  #     if  Employee.find_by(id: session[:employee_id])
+  #       @employee = Employee.find_by(id: session[:employee_id])
+  #       # binding.pry
+  #     elsif Employer.find_by(id: session[:employer_id])
+  #       @employer = current_user
+  #     else
+  #       redirect_to root_path
+  #     end
+  #   end
+  #
+  # end
+
+  private
 
 
 
 
     # @current_uemployee ||= Employee.find_by_id(session[:employee_id]) unless session[:employee_id] == nil
-    # def is_logged_in
-    #   redirect_to root_path  unless current_user
-    # end
+    def logged_in
+      redirect_to root_path  unless current_user
+    end
 
 end
