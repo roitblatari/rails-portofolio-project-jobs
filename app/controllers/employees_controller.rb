b547cda5c62e3f3c43967c290d1b216b280501e9
roitblatari <roitblatari@gmail.com>
2018-07-19 20:54:46 -0400

    befor_action logged_in & update #create method

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,6 +1,9 @@
 class EmployeesController < ApplicationController
-  before_action :logged_in
-  skip_before_action :logged_in, only: [:new, :create, :index]
+  before_action :current_user
+  before_action :require_logged_in, except: [:new, :create]
+ 
+  # before_action :logged_in
+  # skip_before_action :logged_in, only: [:new, :create]
 
   def new
     @employee = Employee.new
@@ -8,7 +11,8 @@ class EmployeesController < ApplicationController
 
   def create
     @employee = Employee.new(employee_params)
-    if @employee.save
+
+    if @employee.save 
       session[:employee_id] = @employee.id
       redirect_to employee_path(@employee)
     else
@@ -16,12 +20,12 @@ class EmployeesController < ApplicationController
     end
   end
 
-  def show
-    @current_user = current_user
-    # binding.pry
-    @employee = Employee.find_by(id: params[:id])
+  def show 
+  @employee = Employee.find_by(id: params[:id])
+    if  current_user != @employee 
+      redirect_to employee_path(@current_user)
+    end
   end
-
 
 private
   def employee_params
--- app/controllers/employers_controller.rb
+++ app/controllers/employers_controller.rb
@@ -1,6 +1,9 @@
 class EmployersController < ApplicationController
-  before_action :logged_in
-  skip_before_action :logged_in, only: [:new, :create, :index]
+  before_action :current_user
+  before_action :require_logged_in, except: [:new, :create, :home]
+ 
+  # before_action :logged_in
+  # skip_before_action :logged_in, only: [:new, :create]
 
   def new
     @employer = Employer.new
@@ -10,7 +13,7 @@ class EmployersController < ApplicationController
     @employer = Employer.new(employer_params)
     if @employer.save
       session[:employer_id] = @employer.id
-      redirect_to employer_path(@employer)
+      redirect_to employer_path(current_user)
     else
       redirect_to root_path
     end
@@ -18,7 +21,11 @@ class EmployersController < ApplicationController
 
   def show
     @employer = Employer.find_by(id: params[:id])
+    if  current_user != @employer
+         redirect_to employer_path(@current_user)
+      end
   end
+
 
 
   private
