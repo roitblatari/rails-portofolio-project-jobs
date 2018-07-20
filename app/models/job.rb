c245348a200e25bc29f2f8d1b28aa56a424f60dd
roitblatari <roitblatari@gmail.com>
2018-07-19 20:47:11 -0400

    validates

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,4 +1,9 @@
 class Job < ApplicationRecord
   belongs_to :employee, optional: true 
   belongs_to :employer
+
+  validates :title, presence: :true
+  validates :address, presence: :true
+  validates :state, presence: :true
+  validates :date, presence: :true
 end
