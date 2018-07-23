df0536cc96af697af93b8442bfd73d45109fe397
roitblatari <roitblatari@gmail.com>
2018-07-23 16:04:25 -0400

    learning scope

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,9 +1,16 @@
 class Job < ApplicationRecord
-  belongs_to :employee, optional: true 
+  belongs_to :employee, optional: true
   belongs_to :employer
 
   validates :title, presence: :true
   validates :address, presence: :true
   validates :state, presence: :true
   validates :date, presence: :true
+
+  # scope :future_jobs , joins(:table).where('.field = ?', 'value')
+  scope :future_jobs, -> (date) { where("date > ?", Date.today) }
+  scope :old_jobs, -> (date) { where("date < ?", Date.today) }
+  # scope :future_jobs, lambda { where("date > ?", Date.today) }
+  # scope :future_jobs, where(:date => Date.today)
+  # scope :name, lambda { |param| where(:field => "value") }
 end
