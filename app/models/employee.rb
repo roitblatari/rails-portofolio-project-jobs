70d9d32b36dbd1fce8cc84bbf377d56540df08a6
roitblatari <roitblatari@gmail.com>
2018-07-25 13:40:00 -0400

    validations

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,5 +1,13 @@
 class Employee < ApplicationRecord
   has_many :jobs
   has_many :employers, through: :jobs
+
+
+    validates :name, presence: :true
+    validates :email, presence: :true
+    # validates :profession, presence: :true
+
+  validates :email, uniqueness: true
   has_secure_password
+
 end
--- app/models/employer.rb
+++ app/models/employer.rb
@@ -1,5 +1,13 @@
 class Employer < ApplicationRecord
   has_many :jobs
   has_many :employees, through: :jobs
+
+  validates :name, presence: :true
+  validates :email, presence: :true
+  # validates :profession, presence: :true
+
+
+  validates :email, uniqueness: true
   has_secure_password
+
 end
