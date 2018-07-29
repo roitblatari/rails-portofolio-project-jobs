5769f0f075a6e9ea7b9d7a6f7128e5305e0ab90d
roitblatari <roitblatari@gmail.com>
2018-07-29 12:33:53 -0400

    no changes

--- app/models/employer.rb
+++ app/models/employer.rb
@@ -1,7 +1,8 @@
 class Employer < ApplicationRecord
   has_many :jobs
   has_many :employees, through: :jobs
-  # validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
+
+
   validates :name, presence: :true
   validates :email, presence: :true
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
