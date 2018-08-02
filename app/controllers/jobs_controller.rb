322b56155b340aca7ee729e6a617b62d88eba3dd
roitblatari <roitblatari@gmail.com>
2018-08-01 23:16:42 -0400

    job params

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,7 +1,5 @@
 class JobsController < ApplicationController
-  # before_action :employer, only: [new]
-  # before_action :logged_in
-  # skip_before_action :logged_in, only: [:new, :create, :show, :index ]
+
   def new
     @employer = Employer.find_by(id: params[:employer_id])
     @job = Job.new
@@ -10,7 +8,7 @@ class JobsController < ApplicationController
 
   def create
     @employer = Employer.find_by(id: params[:employer_id])
-    @job = Job.new(title: params[:job][:title], address: params[:job][:address], state: params[:job][:state],date: params[:job][:date])
+    @job = Job.new(job_params)
     @job.employer = @employer
     if @job.save
       redirect_to employer_job_path(@employer, @job)
@@ -19,44 +17,50 @@ class JobsController < ApplicationController
     end
   end
 
-def show
-  # binding.pry
-  @job = Job.find_by(id: params[:id])
-  @employer = Employer.find_by(id: params[:employer_id])
-  @employee = Employee.find_by(id: params[:employee_id])
-  if @employee
-    @job.employee = @employee
-    # @job.update(employee_id: params[:employee_id])
+  def show
+    @job = Job.find_by(id: params[:id])
+    @employer = Employer.find_by(id: params[:employer_id])
+    @employee = Employee.find_by(id: params[:employee_id])
   end
-end
 
-def index
-  # @employer = Employer.find_by(id: params[:employer_id])
-  @employee = Employee.find_by(id: params[:employee_id])
-  @jobs = Job.all
-end
+  def take_job
+    @employee = Employee.find_by(id: params[:employee_id])
+    @job = Job.find_by(id: params[:job_id])
+    @job.update(employee_id: params[:employee_id])
 
-def upcoming_jobs
-  # binding.pry
-  # @jobs = Job.future_jobs(Time.now)
-  @employee = Employee.find_by(id: params[:employee_id])
-  @jobs = Job.future_jobs(Date.today)
-end
+    redirect_to employee_job_path(@employee, @job)
+  end
 
-def destroy
-  # binding.pry
-  @employee = Employee.find_by(id: params[:employee_id])
-  @job = Job.find_by(id: params[:job_id])
-  @job.destroy
+  def index
+    # @employer = Employer.find_by(id: params[:employer_id])
+    @employee = Employee.find_by(id: params[:employee_id])
+    @jobs = Job.all
+  end
 
-  redirect_to employee_jobs_path(@employee)
-end
+  def upcoming_jobs
+    @employee = Employee.find_by(id: params[:employee_id])
+    @jobs = Job.future_jobs(Date.today)
+  end
+
+  def my_created_jobs
+    @employer = Employer.find_by(id: params[:employer_id])
+    @jobs = @employer.jobs
+  end
+
+  def destroy
+    # binding.pry
+    @employee = Employee.find_by(id: params[:employee_id])
+    @job = Job.find_by(id: params[:job_id])
+    @job.destroy
+
+    redirect_to employee_jobs_path(@employee)
+  end
 
 private
 
-  # def job_params
-  #   params.require(:job).permit( :employer_id, :title, :address, :state, :date)
-  # end
+  def job_params
+    params.require(:job).permit(:title, :address, :state, :date)
+  end
 
   # def employer
   #   @employer = Employer.find_by(id: params[:employer_id])
